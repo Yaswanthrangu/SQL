9a6b9db7808eb58d109088392490c9951e2db26f
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2024-11-13 00:36:33 +0530

    Time: 20 ms (47.04%), Space: 44.8 MB (20.74%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,15 +1,57 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-        long ans = 0;
-        for(int i = 0; i < n; i++) {
-            int largest = nums[i], smallest = nums[i];
-            for(int j = i; j < n; j++) {
-                largest = Math.max(largest, nums[j]);
-                smallest = Math.min(smallest, nums[j]);
-                ans += largest - smallest;
+        
+        long[] lesserLeft = new long[n];
+        long[] lesserRight = new long[n];
+        long[] greaterLeft = new long[n];
+        long[] greaterRight = new long[n];
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            long count = 1;
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
+                count += lesserLeft[stack.pop()];
             }
+            lesserLeft[i] = count;
+            stack.push(i);
         }
+
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            long count = 1;
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+                count += lesserRight[stack.pop()];
+            }
+            lesserRight[i] = count;
+            stack.push(i);
+        }
+
+        stack.clear();
+        for (int i = 0; i < n; i++) {
+            long count = 1;
+            while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
+                count += greaterLeft[stack.pop()];
+            }
+            greaterLeft[i] = count;
+            stack.push(i);
+        }
+
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            long count = 1;
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                count += greaterRight[stack.pop()];
+            }
+            greaterRight[i] = count;
+            stack.push(i);
+        }
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += ((lesserLeft[i] * lesserRight[i]) - (greaterLeft[i] * greaterRight[i])) * nums[i];
+        }
+        
         return ans;
     }
 }
